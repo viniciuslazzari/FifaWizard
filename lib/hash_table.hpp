6bef6c1e2d39e0c28fee07e92170e9ea842580f5
Vinícius Lazzari <vinilazzari028@gmail.com>
2022-08-24 00:49:14 -0300

    ♻️🎨 Update hash table arguments

--- lib/hash_table.hpp
+++ lib/hash_table.hpp
@@ -3,23 +3,24 @@
 
 #include "binary_tree.hpp"
 
-const int size = 1000;
-
 template <typename TKey, class TData>
 class HashTable {
     public:
+        int size;
         Node<TKey, TData>** map;
-        int (*hashFunction)(TKey);
+        int (*hashFunction)(int size, TKey);
         void (*insertHash)(HashTable<TKey, TData>*, TKey, TData);
 
-        HashTable(int (*hashFunction)(TKey), void (*insertHash)(HashTable<TKey, TData>*, TKey, TData));
+        HashTable(int size, int (*hashFunction)(int, TKey), void (*insertHash)(HashTable<TKey, TData>*, TKey, TData));
 
         void printBlock(const int block);
         bool keyExists(const TKey block);
+        Node<TKey, TData>* findKeyNodeAddress(const TKey key);
 };
 
 template <typename TKey, class TData>
-HashTable<TKey, TData>::HashTable(int (*hashFunction)(TKey), void (*insertHash)(HashTable<TKey, TData>*, TKey, TData)){
+HashTable<TKey, TData>::HashTable(int size, int (*hashFunction)(int, TKey), void (*insertHash)(HashTable<TKey, TData>*, TKey, TData)){
+    this->size = size;
     this->hashFunction = hashFunction;
     this->insertHash = insertHash;
 
@@ -33,9 +34,16 @@ void HashTable<TKey, TData>::printBlock(const int key){
 
 template <typename TKey, class TData>
 bool HashTable<TKey, TData>::keyExists(const TKey key){
-    const int hash_position = (*hashFunction)(key);
+    const int hash_position = (*hashFunction)(this->size, key);
 
     return getNodeAddress(map[hash_position], key) != nullptr;
+}
+
+template <typename TKey, class TData>
+Node<TKey, TData>* HashTable<TKey, TData>::findKeyNodeAddress(const TKey key){
+    const int hash_position = (*hashFunction)(this->size, key);
+
+    return getNodeAddress(map[hash_position], key);
 }
 
 #endif
