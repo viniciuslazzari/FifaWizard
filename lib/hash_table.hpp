9fe48f81ee3c4dfab48ad863b9974d9510c07d32
Vinícius Lazzari <vinilazzari028@gmail.com>
2022-08-30 23:36:58 -0300

    🔥 Remove unused function

--- lib/hash_table.hpp
+++ lib/hash_table.hpp
@@ -2,6 +2,7 @@
 #define hash_table_h
 
 #include "binary_tree.hpp"
+#include <iomanip>
 
 template <typename TKey, class TData>
 class HashTable {
@@ -14,8 +15,21 @@ class HashTable {
         HashTable(int size, int (*hashFunction)(int, TKey), void (*insertHash)(HashTable<TKey, TData>*, TKey, TData));
 
         void printBlock(const int block);
-        bool keyExists(const TKey block);
         Node<TKey, TData>* findKeyNodeAddress(const TKey key);
+
+        void returnOccupation(){
+            double count = 0;
+
+            for(int i = 0; i < size; i++){
+                if (this->map[i]) {
+                    count++;
+                }
+            }
+
+            double ocupation = count / this->size;
+
+            std::cout << ocupation << std::fixed << std::setprecision(5) << std::endl;
+        }
 };
 
 template <typename TKey, class TData>
@@ -30,13 +44,6 @@ HashTable<TKey, TData>::HashTable(int size, int (*hashFunction)(int, TKey), void
 template <typename TKey, class TData>
 void HashTable<TKey, TData>::printBlock(const int key){
     printTree(map[key]);
-}
-
-template <typename TKey, class TData>
-bool HashTable<TKey, TData>::keyExists(const TKey key){
-    const int hash_position = (*hashFunction)(this->size, key);
-
-    return getNodeAddress(map[hash_position], key) != nullptr;
 }
 
 template <typename TKey, class TData>
